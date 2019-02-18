999a352dd2f976ea07a6ec1a6b528ba5bb1c445b
Vigneshvikiy <46769037+Vigneshvikiy@users.noreply.github.com>
2019-02-18 20:37:54 +0530

    Create nk.c

---
+++ nk.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i,j,sum=0;
+printf("enter the values:");
+scanf("%d\t%d",&a,&b);
+for(i=1;i<=a;i++)
+{
+printf("%d\n",a);
+}
+for(j=1;j<=b;j++)
+{
+sum=sum+j;
+}
+printf("sum is %d",sum");
+return 0;
+}
